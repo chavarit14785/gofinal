a8bf08513f465bc72f44680583648129200d98fc
Chavarit Yongyutampai <69349543+chavarit14785@users.noreply.github.com>
2020-08-29 14:42:42 +0700

    add new package

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/chavarit14785/gofinal/customers"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("customer service")
+	r := setRouter()
+	r.Run(":2009")
 	//run port ":2009"
+}
+func setRouter() *gin.Engine {
+	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		auth := c.GetHeader("Authorization")
+		if auth != "November 10, 2009" {
+			c.JSON(http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized))
+			c.Abort()
+			return
+		}
+		c.Next()
+	})
+	r.POST("/customers", customers.PostCreateCustomerHandler)
+	r.GET("/customers/:id", customers.GetCustomerByIDHandler)
+	r.GET("/customers", customers.GetAllCustomerHandler)
+	r.PUT("/customers/:id", customers.PutUpdateCustomerHandler)
+	r.DELETE("/customers/:id", customers.DeleteCustomerByIDHandler)
+	return r
 }
